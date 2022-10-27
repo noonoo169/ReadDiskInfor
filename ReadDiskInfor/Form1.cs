eceb047bbdcb2cab881c50292ea953913aad6087
NOO <ta642379@gmail.com>
2022-10-27 10:03:38 +0700

    Scan IP test1

--- ReadDiskInfor/Form1.cs
+++ ReadDiskInfor/Form1.cs
@@ -15,7 +15,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ReadDiskInfor
+namespace ReadDiskInfor   
 {
     public partial class Form1 : Form
     {   
